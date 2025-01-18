63473938976f1372f68f055fe19fbd5a97d336d2
Soham More <141304014+SohamMore100@users.noreply.github.com>
2025-01-18 14:20:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ app.delete("/posts/:id",(req,res)=>{
 })
 
 // app.get("/",(req,res)
-//     res.send("Serving
+//     res.send("Serv
 
 app.listen(port,() => {
     console.log(`listening to Port :${port}`);
